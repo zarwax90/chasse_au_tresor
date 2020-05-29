61504bfeca686fe75b1dfbbb155f6d74302fefa4
zarwax90 <65073852+zarwax90@users.noreply.github.com>
2020-05-29 10:36:22 +0200

    suppression lignes debug

--- class.js
+++ class.js
@@ -11,7 +11,4 @@ class Object {
 }
 
 let boat = new Object("bateau", "5 coups restants", "malus", objets[0]);
-console.log(boat);
 let tresors = new Object("tresor", "win the game", "bonus", objets[1]);
-console.log(tresors)
-console.log(tresors.getxy())
